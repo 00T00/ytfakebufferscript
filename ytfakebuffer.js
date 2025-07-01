72902ffc3e3814d2e1672121589049c9acc5b561
00T00 <toskuipers@hotmail.com>
2025-07-01 20:51:33 +0200

    Add files via upload

---
+++ ytfakebuffer.js
@@ -0,0 +1,9 @@
+(() => {
+    const e = {
+        apply: (e, n, arguments) => {
+            let t = Reflect.apply(e, n, arguments);
+            return 3 === arguments.length && t?.body && "string" == typeof t.body && !t.body.includes(`"isInlinePlaybackNoAd":true`) && (t.body = t.body.replace(`"contentPlaybackContext":{`, `"contentPlaybackContext":{"isInlinePlaybackNoAd":true,`)), t
+        }
+    };
+    window.Object.assign = new Proxy(window.Object.assign, e)
+})();
