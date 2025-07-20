9b7ace6e08883f5efd6e9cb857588c2d60f9b2cd
00T00 <toskuipers@hotmail.com>
2025-07-20 05:23:48 +0200

    Update ytfakebuffer.js

--- ytfakebuffer.js
+++ ytfakebuffer.js
@@ -8,15 +8,3 @@ ytfakebuffer.js text/javascript
     };
     window.Object.assign = new Proxy(window.Object.assign, e)
 })();
-
-
-ytfakebuffer2.js text/javascript
-(() => {
-    const e = {
-        apply: (e, n, arguments) => {
-            let t = Reflect.apply(e, n, arguments);
-            return 3 === arguments.length && t.body && "string" == typeof t.body && !t.body.includes(`"isInlinePlaybackNoAd":true`) && (t.body = t.body.replace(`"contentPlaybackContext":{`, `"contentPlaybackContext":{"isInlinePlaybackNoAd":true,`)), t
-        }
-    };
-    window.Object.assign = new Proxy(window.Object.assign, e)
-})();
