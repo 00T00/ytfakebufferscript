736ca6ff76e87fdab7c0b23cc4f254621645c72b
00T00 <toskuipers@hotmail.com>
2025-07-20 05:16:21 +0200

    Update ytfakebuffer.js

--- ytfakebuffer.js
+++ ytfakebuffer.js
@@ -1,10 +1,21 @@
 ytfakebuffer.js text/javascript
 (() => {
-    alert("Hello! I am an alert box!!");
     const e = {
         apply: (e, n, arguments) => {
             let t = Reflect.apply(e, n, arguments);
             return 3 === arguments.length && t?.body && "string" == typeof t.body && !t.body.includes(`"isInlinePlaybackNoAd":true`) && (t.body = t.body.replace(`"contentPlaybackContext":{`, `"contentPlaybackContext":{"isInlinePlaybackNoAd":true,`)), t
+        }
+    };
+    window.Object.assign = new Proxy(window.Object.assign, e)
+})();
+
+
+ytfakebuffer2.js text/javascript
+(() => {
+    const e = {
+        apply: (e, n, arguments) => {
+            let t = Reflect.apply(e, n, arguments);
+            return 3 === arguments.length && t.body && "string" == typeof t.body && !t.body.includes(`"isInlinePlaybackNoAd":true`) && (t.body = t.body.replace(`"contentPlaybackContext":{`, `"contentPlaybackContext":{"isInlinePlaybackNoAd":true,`)), t
         }
     };
     window.Object.assign = new Proxy(window.Object.assign, e)
